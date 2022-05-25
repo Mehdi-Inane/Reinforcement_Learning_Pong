9d714e54e622e9554275eab3c974448381059ceb
Mehdi-i15 <98083626+Mehdi-i15@users.noreply.github.com>
2022-05-25 20:19:51 +0100

    Add files via upload

---
+++ dqn.py
@@ -0,0 +1,138 @@
+import random
+import numpy
+import gym
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import namedtuple
+import math
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Store Transitions
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, obs, action, next_obs, reward):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+
+        self.memory[self.position] = (obs, action, next_obs, reward)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        """
+        Samples batch_size transitions from the replay memory and returns a tuple
+            (obs, action, next_obs, reward)
+        """
+        sample = random.sample(self.memory, batch_size)
+        return tuple(zip(*sample))
+
+
+steps_done = 0
+
+
+class DQN(nn.Module):
+    def __init__(self, env_config):
+        super(DQN, self).__init__()
+
+        # Save hyperparameters needed in the DQN class.
+        self.batch_size = env_config["batch_size"]
+        self.gamma = env_config["gamma"]
+        self.eps_start = env_config["eps_start"]
+        self.eps_end = env_config["eps_end"]
+        self.anneal_length = env_config["anneal_length"]
+        self.n_actions = env_config["n_actions"]
+        self.steps_done = 0
+        self.conv1  =  nn.Conv2d( 4 , 32 , kernel_size = 8 , stride = 4 , padding = 0 )
+        self.conv2  =  nn.Conv2d( 32 , 64 , kernel_size = 4 , stride = 2 , padding = 0 )
+        self.conv3  =  nn.Conv2d ( 64 , 64 , kernel_size = 3 , stride = 1 , padding = 0 )
+        self.fc1  =  nn.Linear( 3136 , 512 )
+        self.fc2  =  nn.Linear( 512 , self.n_actions )
+
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        """Runs the forward pass of the NN depending on architecture."""
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+    def act(self, observation, exploit=False):
+        """Selects an action with an epsilon-greedy exploration strategy."""
+        # TODO: Implement action selection using the Deep Q-network. This function
+        #       takes an observation tensor and should return a tensor of actions.
+        #       For example, if the state dimension is 4 and the batch size is 32,
+        #       the input would be a [32, 4] tensor and the output a [32, 1] tensor.
+        # TODO: Implement epsilon-greedy exploration.
+        global steps_done
+        if exploit:
+            with torch.no_grad():
+                return self.forward(observation[0]).max(0)[1]
+        action = []
+        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * math.exp(-1. * steps_done / 200)
+        steps_done += 1
+        for state in observation:
+            if random.random() <= eps_threshold:
+                action_index = random.randrange(0, self.n_actions)
+                action.append(action_index)
+            else:
+                action.append(self.forward(state).max(0)[1].item())
+        return torch.tensor(action)
+
+
+def optimize(dqn, target_dqn, memory, optimizer):
+    """This function samples a batch from the replay buffer and optimizes the Q-network."""
+    # If we don't have enough transitions stored yet, we don't train.
+    if len(memory) < dqn.batch_size:
+        return
+
+    # TODO: Sample a batch from the replay memory and concatenate so that there are
+    #       four tensors in total: observations, actions, next observations and rewards.
+    #       Remember to move them to GPU if it is available, e.g., by using Tensor.to(device).
+    #       Note that special care is needed for terminal transitions!
+
+    transitions = memory.sample(dqn.batch_size)
+    Transition = namedtuple('Transition',
+                            ('obs', 'action', 'next_obs', 'reward'))
+    batch = Transition(*transitions)
+    # print(batch.next_obs)
+    non_final_mask = torch.tensor(tuple(map(lambda s: not isinstance(s, numpy.ndarray),
+                                            batch.next_obs)), device=device, dtype=torch.bool)
+
+    non_final_next_states = torch.cat([s for s in batch.next_obs if not isinstance(s, numpy.ndarray)])
+    state_batch = torch.cat(batch.obs)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # TODO: Compute the current estimates of the Q-values for each state-action
+    #       pair (s,a). Here, torch.gather() is useful for selecting the Q-values
+    #       corresponding to the chosen actions.
+    q_values = dqn(state_batch).gather(1, action_batch.unsqueeze(1))
+
+    # TODO: Compute the Q-value targets. Only do this for non-terminal transitions!
+
+    next_state_values = torch.zeros(dqn.batch_size, device=device)
+    next_state_values[non_final_mask] = target_dqn(non_final_next_states).detach().max(1)[0]
+    # Compute the expected Q values
+    q_value_targets = (next_state_values * dqn.gamma) + reward_batch
+    # Compute loss.
+    loss = F.mse_loss(q_values.squeeze(), q_value_targets)
+
+    # Perform gradient descent.
+    optimizer.zero_grad()
+
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
---
+++ train.py
@@ -0,0 +1,101 @@
+import argparse
+
+import gym
+import torch
+import torch.nn as nn
+
+import config
+from utils import preprocess
+from evaluate import evaluate_policy
+from dqn import DQN, ReplayMemory, optimize
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', choices=['CartPole-v0','Pong-v0'])
+parser.add_argument('--evaluate_freq', type=int, default=25, help='How often to run evaluation.', nargs='?')
+parser.add_argument('--evaluation_episodes', type=int, default=5, help='Number of evaluation episodes.', nargs='?')
+
+# Hyperparameter configurations for different environments. See config.py.
+ENV_CONFIGS = {
+    'CartPole-v0': config.CartPole,
+    'Pong-v0' : config.Pong,
+}
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    # Initialize environment and config.
+    args.env = "Pong-v0"
+    env = gym.make(args.env).unwrapped
+    env_config = ENV_CONFIGS["Pong-v0"]
+    #Preprocessing the environment and scaling observations to [0,1]
+    env  =  gym.wrappers.AtariPreprocessing ( env , screen_size = 84 , grayscale_obs = True , frame_skip = 1 , noop_max = 30,scale_obs=True )
+    # Initialize deep Q-networks.
+    dqn = DQN(env_config=env_config).to(device)
+    target_dqn = DQN(env_config=env_config).to(device)
+    target_dqn.load_state_dict(dqn.state_dict())
+    target_dqn.eval()
+    print(env_config["train_frequency"])
+    # Create replay memory.
+    memory = ReplayMemory(env_config['memory_size'])
+
+    # Initialize optimizer used for training the DQN. We use Adam rather than RMSProp.
+    optimizer = torch.optim.Adam(dqn.parameters(), lr=env_config['lr'])
+
+    # Keep track of best evaluation mean return achieved so far.
+    best_mean_return = -float("Inf")
+
+    for episode in range(env_config['n_episodes']):
+        done = False
+
+        obs = preprocess(env.reset(), env=args.env).unsqueeze(0)
+        #updating the frame stack
+        obs_stack  =  torch.cat(env_config["obs_stack_size"]  * [obs]).unsqueeze (0).to(device)
+        print("shape of obs stack",obs_stack.shape)
+        obs = torch.cat((obs_stack[:, 1 :, ...],obs.unsqueeze( 1 )), dim = 1 ).to( device )
+        print("shape of obs",obs.shape)
+        steps = 0
+        while not done:
+            steps += 1
+
+            action = dqn.act(obs)
+
+            # Act in the true environment.
+            
+            obs_old = obs
+            obs, reward, done, info = env.step(action.item())
+            print(obs)
+            # Preprocess incoming observation.
+            if not done:
+                obs = preprocess(obs, env=args.env).unsqueeze(0)
+            
+            next_obs_stack  =  torch.cat((obs_stack[:, 1 :, ...],obs.unsqueeze( 1 )), dim = 1 ).to( device )
+            # TODO: Add the transition to the replay memory. Remember to convert
+            #       everything to PyTorch tensors!
+            reward = torch.tensor([reward], device=device)
+            memory.push(obs_old, action, next_obs_stack, reward)
+
+            # TODO: Run DQN.optimize() every env_config["train_frequency"] steps.
+            if episode % env_config["train_frequency"] == 0:
+                optimize(dqn, target_dqn, memory, optimizer)
+
+            # TODO: Update the target network every env_config["target_update_frequency"] steps.
+            if episode % env_config["target_update_frequency"] == 0:
+                target_dqn.load_state_dict(dqn.state_dict())
+
+        # Evaluate the current agent.
+        if episode % args.evaluate_freq == 0:
+            mean_return = evaluate_policy(dqn, env, env_config, args, n_episodes=args.evaluation_episodes)
+
+            print(f'Episode {episode}/{env_config["n_episodes"]}: {mean_return}')
+
+            # Save current agent if it has the best performance so far.
+            if mean_return >= best_mean_return:
+                best_mean_return = mean_return
+
+                print('Best performance so far! Saving model.')
+                torch.save(dqn, f'models/{args.env}_best.pt')
+
+    # Close environment after training is completed.
+    env.close()
