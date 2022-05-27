1a0dc97e0c082e849b78e8b7dfb6cc61293cbeb7
Mehdi-i15 <98083626+Mehdi-i15@users.noreply.github.com>
2022-05-27 13:27:52 +0100

    Implementing pre-processing and frame stacking

--- utils.py
+++ utils.py
@@ -3,12 +3,21 @@ import random
 import gym
 import torch
 import torch.nn as nn
+import config
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def preprocess(obs, env):
     """Performs necessary observation preprocessing."""
-    if env in ['CartPole-v0','Pong-v0']:
-        return torch.tensor(obs, device=device).float()
+    if env in ['CartPole-v0']:
+        return torch.tensor(obs, device=device).float().unsqueeze(0)
+    elif env in ['Pong-v0']:
+        env_config = config.Pong
+        obs = torch.tensor(obs, device=device).float()
+        obs = obs.unsqueeze(0)
+        #updating the frame stack
+        obs_stack  =  torch.cat(env_config["obs_stack_size"]  * [obs]).unsqueeze(0).to(device)
+        obs = torch.cat((obs_stack[:,1:, ...],obs.unsqueeze(1)), dim = 1 ).to(device)
+        return obs
     else:
         raise ValueError('Please add necessary observation preprocessing instructions to preprocess() in utils.py.')
